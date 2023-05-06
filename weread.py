bec602f6f7e9438cf11a3521b06887e4dea69fc6
lwb <1095274971@qq.com>
2023-05-06 10:15:05 +0800

    init

--- weread.py
+++ weread.py
@@ -1,15 +1,15 @@
 import re
 import requests as req
-import os
-import sys
+from pathlib import Path
 
 openapi = ""
-path = os.path.dirname(sys.argv[0])
-with open(path + '/weread.txt', 'r', encoding='utf-8') as file:
+path = Path(__file__).resolve().parent / 'weread.txt'
+
+with open(path, 'r', encoding='utf-8') as file:
     data = file.read()
     data = data.replace('>>', '>')
     title = re.findall('《(.*)》', data)[0]
     content = '#阅读 #{} \n{}'.format(title, data)
 #    print(content)
     resp = req.post(openapi, json={"content": content})
-    print(resp.status_code)
+    print('推送成功' if resp.status_code != '200' else '推送失败')
